bc213f91154632b619d44e98937136e0cddc7e86
Michael Borde <michael.borde@gmail.com>
2013-12-20 14:31:35 +0100

    fizzbuzz multiple if/else refactoring

--- examples/fizzbuzz/fizzbuzz.sh
+++ examples/fizzbuzz/fizzbuzz.sh
@@ -1,24 +1,36 @@
 function fizzbuzz_enumerate() {
 	local result=()
 	local i; for i in {1..100}; do
-		result+=($(_getValueForNumber ${i}))
+		result+=($(_getValueForNumber "${i}"))
 	done
 	printf "%s " "${result[@]}"
 }
 
 function _getValueForNumber() {
-	local result=${1}
-	if _numberIsMultipleOfTheOtherNumber ${result} 15; then
-		printf "FizzBuzz"
-	elif _numberIsMultipleOfTheOtherNumber ${result} 3; then
-		printf "Fizz"
-	elif _numberIsMultipleOfTheOtherNumber ${result} 5; then
-		printf "Buzz"
+	local number=${1}
+	local result=$(_printValueIfNumberIsMultipleOfDivisor "Fizz" "${number}" 3)
+	result+=$(_printValueIfNumberIsMultipleOfDivisor "Buzz" "${number}" 5)
+	_printValueOfDefaultIfEmpty "${result}" "${number}"
+}
+
+function _printValueIfNumberIsMultipleOfDivisor() {
+	local value=${1}; local number=${2}; local divisor=${3}
+	if _numberIsMultipleOfTheOtherNumber "${number}" "${divisor}"; then
+		printf "${value}"
 	else
-		printf ${result}
+		printf ""
 	fi
 }
 
 function _numberIsMultipleOfTheOtherNumber() {
 	(( ${1}%${2} == 0 ))
+}
+
+function _printValueOfDefaultIfEmpty() {
+	local value=${1}; local default=${2}
+	if [[ "${value}" != "" ]]; then
+		printf "${value}"
+	else
+		printf "${default}"
+	fi
 }
