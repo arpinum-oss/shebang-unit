b577ff054a881109a05f4f88a4bc650461b2bcd4
Michael Borde <michael.borde@gmail.com>
2013-12-22 21:25:29 +0100

    array contains and print array

--- sources/production/system.sh
+++ sources/production/system.sh
@@ -15,3 +15,25 @@ function system::print_with_color() {
 	local text=$1; local color=$2; local default_color=$3
 	printf "${color}${text}${default_color}\n"
 }
+
+function system::array_contains() {
+	local value=${1}
+	shift 1;
+	local array=${@}
+	local element; for element in ${array[@]}; do
+		if [[ "${element}" == "${value}" ]]; then
+			return ${SBU_SUCCESS_STATUS_CODE}
+		fi
+	done
+	return ${SBU_FAILURE_STATUS_CODE}
+}
+
+function system::print_array() {
+	local array=("${@}")
+	local array_as_string=""
+	local i; for (( i=0; i < ${#array[@]}; i++ )); do
+		array_as_string+="${array[${i}]}, "
+	done
+	array_as_string=${array_as_string/%, /}
+	printf "[%s]" "${array_as_string}"
+}
--- sources/tests/system_test.sh
+++ sources/tests/system_test.sh
@@ -15,7 +15,7 @@ function the_date_in_seconds_is_a_valid_number() {
 
 	_is_a_number ${number}
 
-	assertion::status_code_is_success ${?} "The actual value <${number}> is not a number"
+	assertion::status_code_is_success $? "The actual value <${number}> is not a number."
 }
 
 function _is_a_number() {
@@ -26,4 +26,36 @@ function can_print_with_color() {
 	local message="$(system::print_with_color "text" "color" "default_color")"
 
 	assertion::equal "colortextdefault_color" "${message}"
+}
+
+function a_contained_value_is_contained_by_the_array() {
+	local array=("a" "b" "c")
+
+	system::array_contains "b" "${array[@]}"
+
+	assertion::status_code_is_success $? "The array does not contain the value."
+}
+
+function a_not_contained_value_is_not_contained_by_the_array() {
+	local array=("a" "b" "c")
+
+	system::array_contains "z" "${array[@]}"
+
+	assertion::status_code_is_failure $? "The array contain the value."
+}
+
+function can_print_array() {
+	local array=("a" "123" "douze")
+
+	local string="$(system::print_array "${array[@]}")"
+
+	assertion::equal "[a, 123, douze]" "${string}"
+}
+
+function can_print_array_with_spaces() {
+	local array=("un deux" "54" "choco lat")
+
+	local string="$(system::print_array "${array[@]}")"
+
+	assertion::equal "[un deux, 54, choco lat]" "${string}"
 }
