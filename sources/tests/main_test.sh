e6f27c4ffac3eb4d7529ffd80b43d4c6d5e71a63
Michael Borde <michael.borde@gmail.com>
2014-01-16 17:03:40 +0100

    usage

--- sources/tests/main_test.sh
+++ sources/tests/main_test.sh
@@ -15,7 +15,7 @@ function can_enable_colors() {
 	assertion__equal "${SBU_YES}" "${SBU_USE_COLORS}"
 }
 
-function can_enable_colors_with_short_argument() {
+function can_enable_colors_with_short_option() {
 	SBU_USE_COLORS="${SBU_NO}"
 
 	main__main -c="${SBU_YES}" "${_TEST_DIRECTORY}" > /dev/null
@@ -39,10 +39,46 @@ function can_use_a_test_file_pattern() {
 	assertion__equal "*my_test.sh" "${SBU_TEST_FILE_PATTERN}"
 }
 
-function can_use_a_test_file_pattern_with_short_argument() {
+function can_use_a_test_file_pattern_with_short_option() {
 	SBU_TEST_FILE_PATTERN="anything"
 
 	main__main -p=*my_test.sh "${_TEST_DIRECTORY}" > /dev/null
 
 	assertion__equal "*my_test.sh" "${SBU_TEST_FILE_PATTERN}"
+}
+
+function cannot_use_unknown_option() {
+  local message
+
+  message="$(main__main --iks=plop "${_TEST_DIRECTORY}")"
+
+  assertion__status_code_is_failure $?
+  assertion__string_contains "${message}" "shebang_unit: illegal option -- iks"
+  assertion__string_contains "${message}" "[usage]"
+}
+
+function cannot_use_unknown_short_option() {
+  local message
+
+  message="$(main__main -x=plop "${_TEST_DIRECTORY}")"
+
+  assertion__string_contains "${message}" "shebang_unit: illegal option -- x"
+}
+
+function can_print_usage_for_help_option() {
+  local message
+
+  message="$(main__main --help)"
+
+  assertion__status_code_is_success $?
+  assertion__string_contains "${message}" "[usage]"
+}
+
+function can_print_usage_for_help_short_option() {
+  local message
+
+  message="$(main__main -h)"
+
+  assertion__status_code_is_success $?
+  assertion__string_contains "${message}" "[usage]"
 }
