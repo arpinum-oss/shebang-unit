762a9bfe2edc37eb5516dc19b150ec74977e153c
Michael Borde <michael.borde@gmail.com>
2014-06-15 16:26:37 +0200

    Suite outputs for junit reporter

--- sources/production/core/reporter/junit_reporter.sh
+++ sources/production/core/reporter/junit_reporter.sh
@@ -11,6 +11,8 @@ function junit_reporter__test_file_starts_running() {
   database__put "sbu_current_suite_name" "${suite_name}"
   _junit_reporter__write_line_to_report \
     "  <testsuite name=\"${suite_name}\" tests=\"${test_count}\">"
+  _junit_reporter__delete_all_outputs_lines "suite"
+  _junit_reporter__redirect_outputs_to_database "suite"
 }
 
 function junit_reporter__global_setup_has_failed() {
@@ -22,7 +24,8 @@ function junit_reporter__test_starts_running() {
   _junit_reporter__write_line_to_report \
     "    <testcase name=\"$1\" classname=\"${suite_name}\" \
 time=\"\${sbu_current_test_time}\">"
-  _junit_reporter__redirect_test_outputs_to_database
+  _junit_reporter__delete_all_outputs_lines "test"
+  _junit_reporter__redirect_outputs_to_database "test"
 }
 
 function junit_reporter__test_has_succeeded() {
@@ -40,8 +43,9 @@ function junit_reporter__test_is_skipped() {
 }
 
 function junit_reporter__test_ends_running() {
+  _junit_reporter__redirect_outputs_to_database "suite"
   _junit_reporter__write_time_in_current_test_case_tag_in_report "$1"
-  _junit_reporter__flush_all_test_outputs_to_report_if_any
+  _junit_reporter__flush_all_outputs_to_report_if_any "test"
   _junit_reporter__write_line_to_report "    </testcase>"
 }
 
@@ -55,6 +59,7 @@ function _junit_reporter__write_time_in_current_test_case_tag_in_report() {
 }
 
 function junit_reporter__test_file_ends_running() {
+  _junit_reporter__flush_all_outputs_to_report_if_any "suite"
   _junit_reporter__write_line_to_report "  </testsuite>"
   database__put "sbu_current_suite_name" ""
 }
@@ -76,41 +81,55 @@ function _junit_reporter__write_line_to_report() {
   system__print_line "$1" >> "${SBU_JUNIT_REPORTER_OUTPUT_FILE}"
 }
 
-function _junit_reporter__redirect_test_outputs_to_database() {
-  database__put "sbu_current_test_standard_ouputs_lines" ""
-  database__put "sbu_current_test_error_ouputs_lines" ""
-  exec 1<>\
-    "$(database__get_descriptor "sbu_current_test_standard_ouputs_lines")"
-  exec 2<>\
-    "$(database__get_descriptor "sbu_current_test_error_ouputs_lines")"
+function _junit_reporter__redirect_outputs_to_database() {
+  local scope=$1
+  exec 1>>\
+    "$(database__get_descriptor "sbu_current_${scope}_standard_ouputs_lines")"
+  exec 2>>\
+    "$(database__get_descriptor "sbu_current_${scope}_error_ouputs_lines")"
 }
 
-function _junit_reporter__flush_all_test_outputs_to_report_if_any() {
-  _junit_reporter__flush_test_outputs_to_report_if_any "standard"
-  _junit_reporter__flush_test_outputs_to_report_if_any "error"
+function _junit_reporter__delete_all_outputs_lines() {
+  database__put "sbu_current_$1_standard_ouputs_lines"
+  database__put "sbu_current_$1_error_ouputs_lines"
 }
 
-function _junit_reporter__flush_test_outputs_to_report_if_any() {
-  local outputs_type=$1
-  local key="sbu_current_test_${outputs_type}_ouputs_lines"
+function _junit_reporter__flush_all_outputs_to_report_if_any() {
+  _junit_reporter__flush_outputs_to_report_if_any "$1" "standard"
+  _junit_reporter__flush_outputs_to_report_if_any "$1" "error"
+}
+
+function _junit_reporter__flush_outputs_to_report_if_any() {
+  local scope=$1
+  local outputs_type=$2
+  local key="sbu_current_${scope}_${outputs_type}_ouputs_lines"
   local outputs="$(database__get "${key}")"
   if [[ -n "${outputs}" ]]; then
-    _junit_reporter__write_test_outputs_to_report "${outputs_type}" "${outputs}"
+    _junit_reporter__write_outputs_to_report \
+      "${scope}" "${outputs_type}" "${outputs}"
     database__put "${key}" ""
   fi
 }
 
-function _junit_reporter__write_test_outputs_to_report() {
-  local outputs_type=$1
-  local outputs=$2
+function _junit_reporter__write_outputs_to_report() {
+  local scope=$1
+  local outputs_type=$2
+  local outputs=$3
   local tag="$(_junit_reporter__get_tag_for_outputs_type "${outputs_type}")"
-  _junit_reporter__write_line_to_report "      <${tag}>"
+  local indentation="$(_junit_reporter__get_indentation_for_scope "${scope}")"
+  _junit_reporter__write_line_to_report "${indentation}<${tag}>"
   _junit_reporter__write_line_to_report "$(xml__encode_text "${outputs}")"
-  _junit_reporter__write_line_to_report "      </${tag}>"
+  _junit_reporter__write_line_to_report "${indentation}</${tag}>"
 }
 
 function _junit_reporter__get_tag_for_outputs_type() {
   [[ "$1" == "standard" ]] \
     && system__print "system-out" \
     || system__print "system-err"
+}
+
+function _junit_reporter__get_indentation_for_scope() {
+  [[ "$1" == "suite" ]] \
+    && system__print "    " \
+    || system__print "      "
 }
